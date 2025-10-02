2312fa7efec5cafabf04e6500d59e9e105559e10
Eloy Dominguez <eloydominguez3@gmail.com>
2025-10-02 18:47:41 +0200

    Ultimos retoques

--- DAOcarreras.py
+++ DAOcarreras.py
@@ -21,8 +21,16 @@ class DAOcarreras:
         self.__class__.cursor('UPDATE carreras SET nombre= %s WHERE nombre=%s',(nuevo_nombre, self.nombre))
         self.__class__.mydb.commit()
 
-    def eliminar_carrera(self,nombre_eliminar)
+    def eliminar_carrera(self,nombre_eliminar):
         self.__class__.cursor('DELETE FROM carreras WHERE nombre=%s', (nombre_eliminar))
+        self.__class__.mydb.commit()
+
+    def mostrar_carreras(self):
+        self.__class__.cursor('SELECT * FROM carreras')
+        resultado = self.__class__.cursor.fetchall()
+        return resultado
+    
+    
     
 
     
