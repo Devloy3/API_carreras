ad2f72dc26f80d57c0084fb08c8e672ebb0130c3
Eloy Dominguez <eloydominguez3@gmail.com>
2025-10-02 18:40:43 +0200

    Añadiendo logica de Daocarreras

---
+++ DAOcarreras.py
@@ -0,0 +1,28 @@
+import mysql.connector
+
+class DAOcarreras:
+    mydb = mysql.connector.connect(
+        host= "localhost",
+        user= "root",
+        paswoord= "123456",
+        database= "carreras"
+    )
+
+    cursor = mydb.cursor()
+
+    def __init__(self,nombre):
+        self.nombre = nombre
+
+    def crear_carrera(self):
+        self.__class__.cursor('INSERT INTO carreras(nombre) VALUES (%)', (self.nombre))
+        self.__class__.mydb.commit()
+
+    def actualizar_carrera(self,nuevo_nombre):
+        self.__class__.cursor('UPDATE carreras SET nombre= %s WHERE nombre=%s',(nuevo_nombre, self.nombre))
+        self.__class__.mydb.commit()
+
+    def eliminar_carrera(self,nombre_eliminar)
+        self.__class__.cursor('DELETE FROM carreras WHERE nombre=%s', (nombre_eliminar))
+    
+
+    
