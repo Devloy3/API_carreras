99a4397e4cd7e9637d750d729ee4efe85a96d044
Àlex Milian <alexmiliandieguez@gmail.com>
2025-10-10 19:27:23 +0200

    Implementación de mostrar carreras correctamente

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def menu():
         opcion = int(input("Introduce una opcion:"))
         
         if opcion == 1:
-            nombre = input("Insertar la carrera que quieras insertar:")
+            nombre = input("Introduce el nombre de la carrera que quieras insertar:")
             try:
                 resp = req.post(f"http://localhost:5000/crear_carrera", data={"nombre":nombre})
                 print(resp.text)
@@ -21,20 +21,21 @@ def menu():
         elif opcion == 2:
             print("\n Carreras \n")
             resp = req.get("http://localhost:5000/mostrar")
-            for i in resp.json():
-                print(i)
+            datos = resp.json()  # ja és llista de diccionaris
+            for carrera in datos:
+                print(f"ID: {carrera['id']}  |  Nombre: {carrera['nombre']}")
         elif opcion == 3: 
-            id = int(input("Introduce el id del cual quieres cambiar:"))
+            nombre_2 = input("Introduce el ID la carrera que desea cambiar:")
             nombre_3 = input("Introduce el nombre actual:")
             try: 
-                req.put(f"http://localhost:5000/modificar_carrera/{id}",data={"nuevo_nombre":nombre_3})
+                req.put(f"http://localhost:5000/modificar_carrera/{nombre_2}",data={"nuevo_nombre":nombre_3})
                 print("Carrera actualizada")
             except:
                 print("Ha habido un fallo en la base de datos")
         elif opcion == 4:
-            ideliminar = int(input("Dime el id del que quieras eliminar:"))
+            nombre_eliminar = input("Introduce el ID que quieres eliminar:")
             try:
-                respuesta = req.delete(f"http://localhost:5000/eliminar/{ideliminar}")
+                respuesta = req.delete(f"http://localhost:5000/eliminar/{nombre_eliminar}")
                 print(respuesta.text)
             except:
                 print("Ha habido un fallo en la base de datos")
